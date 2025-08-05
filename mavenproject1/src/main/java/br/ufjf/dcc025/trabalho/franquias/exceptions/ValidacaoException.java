163ddfd3e201f47ac37801b15371f1541cdc3016
Sara Ingrid <saraingrid2505@gmail.com>
2025-08-05 16:41:09 -0300

    Implementar hierarquia de usuários com herança e polimorfismo + Modelo Franquia e Endereço

---
+++ mavenproject1/src/main/java/br/ufjf/dcc025/trabalho/franquias/exceptions/ValidacaoException.java
@@ -0,0 +1,8 @@
+package br.ufjf.dcc025.trabalho.franquias.exceptions;
+
+public class ValidacaoException extends FranquiaException {
+    
+    public ValidacaoException(String message) {
+        super(message);
+    }
+}
--- mavenproject1/src/main/java/br/ufjf/dcc025/trabalho/franquias/model/franquia/Franquia.java
+++ mavenproject1/src/main/java/br/ufjf/dcc025/trabalho/franquias/model/franquia/Franquia.java
@@ -1,13 +1,92 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package br.ufjf.dcc025.trabalho.franquias.model.franquia;
 
-/**
- *
- * @author sarai
- */
-public class Franquia {
+import br.ufjf.dcc025.trabalho.franquias.model.usuarios.Gerente;
+import java.io.Serializable;
+
+public class Franquia implements Serializable {
+    private Long id;
+    private String nome;
+    private Endereco endereco; // Composição
+    private Gerente gerente;
+    private double receitaAcumulada;
+    private int totalPedidos;
     
+    public Franquia(String nome, Endereco endereco, Gerente gerente) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.gerente = gerente;
+        this.receitaAcumulada = 0.0;
+        this.totalPedidos = 0;
+    }
+    
+    // Método para calcular ticket médio
+    public double getTicketMedio() {
+        return totalPedidos > 0 ? receitaAcumulada / totalPedidos : 0.0;
+    }
+    
+    // Método para adicionar receita
+    public void adicionarReceita(double valor) {
+        this.receitaAcumulada += valor;
+        this.totalPedidos++;
+    }
+    
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public Endereco getEndereco() {
+        return endereco;
+    }
+    
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+    
+    public Gerente getGerente() {
+        return gerente;
+    }
+    
+    public void setGerente(Gerente gerente) {
+        this.gerente = gerente;
+    }
+    
+    public double getReceitaAcumulada() {
+        return receitaAcumulada;
+    }
+    
+    public void setReceitaAcumulada(double receitaAcumulada) {
+        this.receitaAcumulada = receitaAcumulada;
+    }
+    
+    public int getTotalPedidos() {
+        return totalPedidos;
+    }
+    
+    public void setTotalPedidos(int totalPedidos) {
+        this.totalPedidos = totalPedidos;
+    }
+    
+    @Override
+    public String toString() {
+        return "Franquia{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", endereco=" + endereco +
+                ", gerente=" + (gerente != null ? gerente.getNome() : "Não atribuído") +
+                ", receita=" + receitaAcumulada +
+                '}';
+    }
 }
